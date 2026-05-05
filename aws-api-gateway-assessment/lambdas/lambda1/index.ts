8a530ef371ab13dc45557281307ce569715f69e9
Isaiah <i.x.smith137@gmail.com>
2026-05-05 19:40:18 -0400

    Added functionality to retrieve data from Openweathermap service in index.ts Update tsconfig file to use CommonJS module, target ES2020, set esModuleInterop to true, added 'node' to types array, and set verbatimModuleSyntax to false.

---
+++ aws-api-gateway-assessment/lambdas/lambda1/index.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+export const handler = async (event: any) => {
+    try {
+        const city = event.queryStringParameters?.city;
+
+
+        if(!city) {
+            return {
+                statusCode: 400,
+                body: JSON. stringify({ error: "City is required"}),
+            };
+        }
+
+        const apiKey = process.env.OPENWEATHER_API_KEY;
+
+        const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+        );
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                city: response.data.name,
+                temp: response.data.main.temp,
+                description: response.data.weather[0].description,
+            }),
+        };
+    } catch (error: any) {
+        console.error(error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({error: "Internal Server Error"}),
+        };
+    }
+
+};
